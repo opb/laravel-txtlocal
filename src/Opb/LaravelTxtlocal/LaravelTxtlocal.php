a7e7a452ad477b99df0091a00e5f57c513f018d8
Olly Butterfield <olly@opb.me.uk>
2013-08-28 17:21:40 +0100

    Added class file

---
+++ src/Opb/LaravelTxtlocal/LaravelTxtlocal.php
@@ -0,0 +1,42 @@
+<?php namespace Opb\LaravelTxtlocal;
+
+use Illuminate\View\Environment;
+use Illuminate\Support\Facades\Input;
+use Illuminate\Support\Facades\Log;
+
+class LaravelTxtlocal {
+
+	protected $msg = array();
+
+	public function __construct(){}
+
+	public function incoming()
+	{
+		$this->msg['sender'] = Input::get('sender');
+		$this->msg['keyword'] = Input::get('keyword');
+		$this->msg['content'] = Input::get('content');
+		$this->msg['comments'] = Input::get('comments');
+		$this->msg['inNumber'] = Input::get('inNumber');
+	}
+	public function sender()
+	{
+		return trim($this->msg['sender']);
+	}
+	public function keyword()
+	{
+		return trim($this->msg['keyword']);
+	}
+	public function content()
+	{
+		return trim($this->msg['content']);
+	}
+	public function comments()
+	{
+		return trim($this->msg['comments']);
+	}
+	public function inNumber()
+	{
+		return trim($this->msg['inNumber']);
+	}
+
+}
