0fcb50a846ec9ffe9a1adfa52032cada20e19eb7
Olly Butterfield <olly@opb.me.uk>
2013-08-28 16:37:37 +0100

    Added initial workbench files

---
+++ src/Opb/LaravelTxtlocal/LaravelTxtlocalServiceProvider.php
@@ -0,0 +1,34 @@
+<?php namespace Opb\LaravelTxtlocal;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelTxtlocalServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}
